8dd7b3c08350741f1c3f6b8054e0d3f73a8b1c1e
Riley Labrecque <git@rileylabrecque.com>
2016-12-24 23:21:48 -0600

    Reorganized Everything

--- SolitaireAI/Solitaire.cs
+++ SolitaireAI/Solitaire.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+/*using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;*/
+
 
 namespace SolitaireAI {
 	struct State {
@@ -15,6 +19,12 @@ namespace SolitaireAI {
 		}
 
 		public Bitmap Run(Bitmap capture) {
+			//Image<Bgr, Byte> img = new Image<Bgr, Byte>(capture);
+			/*using (Mat img = new Mat(200, 400, DepthType.Cv8U, 3)) {
+				img.SetTo(new Bgr(255, 0, 0).MCvScalar);
+				capture = img.Bitmap;
+			}*/
+
 			capture.DrawRect(new Rectangle(45, 45, 60, 60), Color.FromArgb(120, 0, 0, 255));
 
 			return capture;
